b1854d108b58f40a2cacf62fe35dc87ccc1a464a
Lenin Hasda <leninhasda@gmail.com>
2018-02-21 14:15:39 +0600

    Need to undo this

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Faker\Factory as Faker;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+
+    protected $faker;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+        $this->send();
+    }
+
+    public function tearDown()
+    {
+        $this->artisan('migrate:reset');
+        parent::tearDown();
+    }
 }
